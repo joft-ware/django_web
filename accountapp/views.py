8fd03dc013d03699a7d2e4c65010f5cca6bf6e9b
sky033116 <34998542+sky033116@users.noreply.github.com>
2021-01-02 14:53:24 +0900

    get & post 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,9 +2,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 # Create your views here.
 
+from accountapp.models import hw
+
+
 def hello_world(request):
 
-    if(request.method =="POST"):
-        return render(request, 'accountapp/hello.html', context={'text':'POST METHOD!!!'})
+    if request.method == "POST":
+
+        temp = request.POST.get('hello_world_input')
+
+        news = hw()
+        news.text = temp
+        news.save()
+
+        return render(request, 'accountapp/hello.html', context={'hello_world_output':news})
     else:
         return render(request, 'accountapp/hello.html', context={'text':'GET METHOD!!!'})
