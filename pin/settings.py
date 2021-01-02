37719df3718e98a39235f7b18679bb0be6f759a9
sky033116 <34998542+sky033116@users.noreply.github.com>
2021-01-02 20:44:13 +0900

    Media

--- pin/settings.py
+++ pin/settings.py
@@ -150,3 +150,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
