5e3af4d7c71cc3c089b5f5a8c85a823c3aa5c76c
sky033116 <34998542+sky033116@users.noreply.github.com>
2021-01-02 21:17:37 +0900

    Profile App - CreateView

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,4 +1,8 @@
-app_name = 'profileapp'
-urlpatterns = [
+from django.urls import path
+from profileapp.views import ProfileCreateView
 
+app_name = 'profileapp'
+
+urlpatterns = [
+    path('create/',ProfileCreateView.as_view(), name='create'),
 ]
