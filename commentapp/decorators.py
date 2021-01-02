95dfc7065bb74f6632f2b4a44cc51af25bf4c9dd
sky033116 <34998542+sky033116@users.noreply.github.com>
2021-01-03 02:32:38 +0900

    댓글 기능(+삭제) 추가

---
+++ commentapp/decorators.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.http import HttpResponseForbidden
+
+from commentapp.models import Comment
+
+
+def comment_ownership_required(func) :
+    def decorated(request, *args, **kwargs):
+        comment = Comment.objects.get(pk=kwargs['pk'])
+        if not comment.writer == request.user:
+            return HttpResponseForbidden()
+        return func(request, *args, **kwargs)
+    return decorated
