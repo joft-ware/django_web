52f33b8cc8bc799bf6589d68193595b1c9015db1
sky033116 <34998542+sky033116@users.noreply.github.com>
2021-01-03 17:10:32 +0900

    Project 추가

--- pin/settings.py
+++ pin/settings.py
@@ -44,7 +44,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     'profileapp',
     'articleapp',
     'commentapp',
+    'projectapp',
 ]
 
 MIDDLEWARE = [
